23b74444f2d6850c847193600184da3966491e18
letsdream <403600115@qq.com>
2016-04-11 17:15:10 +0800

    Update init.sh

--- chap03/mysql/init.sh
+++ chap03/mysql/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e                                                  #set -e 表示一旦脚本运行错误就报错并停止执行余下命令
-set -o xtrace                                           #表示会跟踪脚本的执行过程，有利于调试
+set -e                                                              #set -e 表示一旦脚本运行错误就报错并停止执行余下命令
+set -o xtrace                                                       #表示会跟踪脚本的执行过程，有利于调试
 
-TOP_DIR=$(cd $(dirname "$0") && pwd)
-TEMP=`mktemp`; rm -rfv $TEMP >/dev/null; mkdir -p $TEMP;
+TOP_DIR=$(cd $(dirname "$0") && pwd)                                #获取脚本所在路径并赋值给TOP_DIR
+TEMP=`mktemp`; rm -rfv $TEMP >/dev/null; mkdir -p $TEMP;            #利用mktemp在tmp下建立临时文件，
 DEST=/opt/stack
 source $TOP_DIR/tools/function
 cp -rf $TOP_DIR/`ls -l $TOP_DIR/localrc | awk '{print $11}'` /root/
